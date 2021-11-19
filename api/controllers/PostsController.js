b04201cd6230f3956d3934f5f1d8200cb6507851
hemathk7ma4 <khemathlife@gamil.com>
2021-11-19 17:07:22 +0530

    CURD FOR POST

---
+++ api/controllers/PostsController.js
@@ -0,0 +1,56 @@
+/**
+ * PostsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+          posts: async function (req, res) {
+                    Post.find().exec((err, result) => {
+                              if (err) {
+                                        return res.serverError(err.toString())
+                              }
+                              console.log(" finds")
+                              res.send(result);
+
+                    })
+
+          },
+          create: async function (req, res) {
+                    const body = req.allParams()
+                    sails.log.debug(body)
+                    Post.create(body).exec(() => {
+                              console.log("object created")
+                              res.send(body);
+
+                    })
+          },
+
+          findById: async function (req, res) {
+                    const postId = req.param('postId')
+                    sails.log.debug(postId)
+                    Post.findOne({ id: postId }).exec((err, result) => {
+                              if (err) {
+                                        return res.badRequest(err.toString())
+                              }
+                              console.log("object created")
+                              res.send(result);
+
+                    })
+
+          },
+
+          destroy:async  function (req, res) {
+                    const postId = req.param('postId')
+                    sails.log.debug(postId)
+                    Post.destroy({ id: postId }).exec((err, result) => {
+                              if (err) {
+                                        return res.badRequest(err)
+                              }
+                              console.log("object destroyed")
+                              res.send(result);
+                    })
+          }
+};
+
